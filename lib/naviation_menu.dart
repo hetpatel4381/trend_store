2fba46ba4a012bc8b929709e0fbe691bf63f511a
hetpatel4381 <hetpatel2808hp@gmail.com>
2023-12-26 14:41:01 +0530

    created bottom navigation UI

--- lib/naviation_menu.dart
+++ lib/naviation_menu.dart
@@ -1,19 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:trend_store/features/shop/screens/home/home.dart';
+import 'package:trend_store/utils/constants/colors.dart';
+import 'package:trend_store/utils/helpers/helper_functions.dart';
 
 class NavigationMenu extends StatelessWidget {
   const NavigationMenu({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(NavigationController());
+    final darkMode = AppHelperFunctions.isDarkMode(context);
+
     return Scaffold(
-      bottomNavigationBar: NavigationBar(
-        destinations: [
-          Container(color: Colors.green),
-          Container(color: Colors.blue),
-          Container(color: Colors.purple),
-          Container(color: Colors.orange),
-        ],
+      bottomNavigationBar: Obx(
+        () => NavigationBar(
+          height: 80,
+          elevation: 0,
+          selectedIndex: controller.selectedIndex.value,
+          onDestinationSelected: (index) =>
+              controller.selectedIndex.value = index,
+          backgroundColor: darkMode ? AppColors.black : AppColors.white,
+          indicatorColor: darkMode ? AppColors.white.withOpacity(0.1) : AppColors.black.withOpacity(0.1),
+          destinations: const [
+            NavigationDestination(icon: Icon(Iconsax.home), label: 'Home'),
+            NavigationDestination(icon: Icon(Iconsax.shop), label: 'Store'),
+            NavigationDestination(icon: Icon(Iconsax.heart), label: 'Wishlist'),
+            NavigationDestination(icon: Icon(Iconsax.user), label: 'Profile'),
+          ],
+        ),
       ),
+      body: Obx(() => controller.screens[controller.selectedIndex.value]),
     );
   }
+}
+
+class NavigationController extends GetxController {
+  final Rx<int> selectedIndex = 0.obs;
+
+  final screens = [
+    const HomeScreen(),
+    Container(color: Colors.blue),
+    Container(color: Colors.purple),
+    Container(color: Colors.orange),
+  ];
 }
